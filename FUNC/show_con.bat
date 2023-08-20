3e7df1bd25af57aa93cbfb0b9ff57645f07b9188
ilobmirt <ilobmirt@gmail.com>
2023-08-19 22:40:34 -0400

    New direction with the project

--- FUNC/show_con.bat
+++
@@ -1,8 +0,0 @@
-:: FUNC/SHOW_CON.BAT
-:: Displays the project configuration
-::===============================================================================================::
-ECHO "===FUNC:SHOW_CON.BAT==="
-ECHO "USER: %GITHUB_USER%"
-ECHO "PROJECT: %GITHUB_PROJECT%"
-ECHO "BRANCH: %GITHUB_BRANCH%"
-ECHO "DOWNLOAD DIR: %PROJECTS_BASE_DIR%"
